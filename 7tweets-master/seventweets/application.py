fa11f81a9f7546f5978327ae01e39a34bedbdda7
djomlaart <mladenmlazarevic@gmail.com>
2017-12-20 00:02:36 +0100

    restructure master

---
+++ 7tweets-master/seventweets/application.py
@@ -0,0 +1,33 @@
+"""
+Module contains Flask application and routes 
+"""
+from flask import Flask, jsonify, request
+from storage import Storage
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/tweets", methods=['GET'])
+def get_tweets():
+    return jsonify(Storage.get_tweets()), 200
+
+
+@app.route("/tweets/<int:tweet_id>", methods=['GET'])
+def get_tweet(tweet_id):
+    tweet = Storage.get_tweet(tweet_id)
+    return jsonify(tweet), 200 if tweet else 404
+
+
+@app.route("/tweets/", methods=['POST'])
+def post_tweet():
+    tweet = json.loads(request.data)
+    Storage.post_tweet(tweet)
+    return jsonify(tweet), 201
+
+
+@app.route("/tweets/<int:tweet_id>", methods=['DELETE'])
+def del_tweet(tweet_id):
+    tweet = Storage.del_tweet(tweet_id)
+    print(tweet)
+    return jsonify(None), 204 if tweet else 404
